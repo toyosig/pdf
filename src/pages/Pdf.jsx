bd70d6b1867e857af4839dd8147b0203315491ad
toyosig <gadesanya9907@gmail.com>
2024-06-22 18:33:48 +0100

    wow

--- src/pages/Pdf.jsx
+++ src/pages/Pdf.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
+import PdfViewer from '../components/PdfView';
+import pdfFile from '../assets/POWER.pdf';
 
 const Pdf = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="w-full h-full max-w-screen-lg">
-        <iframe
-          src="/POWER.pdf#toolbar=0"
-          title="PDF Viewer"
-          width="100%"
-          height="100%"
-          style={{ border: 'none' }}
-        ></iframe>
-
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PdfViewer fileUrl={pdfFile} />
+        </div>
+    );
 };
 
 export default Pdf;
