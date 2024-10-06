c5c21ca1312efd01d94a246c3a1012c1ed052c79
toyosig <gadesanya9907@gmail.com>
2024-10-06 21:31:20 +0100

    new

--- src/components/PdfView.jsx
+++ src/components/PdfView.jsx
@@ -4,7 +4,7 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const PdfViewer = ({ fileUrl }) => {
     return (
-        <div style={{ height: '750px', width: '100%' }}>
+        <div className="h-screen w-full flex justify-center items-center p-2 md:p-4">
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
                 <Viewer fileUrl={fileUrl} />
             </Worker>
