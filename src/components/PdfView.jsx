5791fca1b39ee4dd4b1d31a5e27e865ff65dd067
toyosig <gadesanya9907@gmail.com>
2024-10-07 08:24:28 +0100

    new

--- src/components/PdfView.jsx
+++ src/components/PdfView.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-import { pdfjs } from 'react-pdf';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PdfViewer = ({ fileUrl }) => {
+    // Default layout plugin for better user experience on mobile
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
     return (
-        <div className=" flex justify-center items-center">
+        <div className="h-screen w-full">
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
                 <Viewer 
                     fileUrl={fileUrl}
-                    theme={{
-                        theme: 'light', // Optional: Use a dark theme for better visibility
-                    }}
+                    plugins={[defaultLayoutPluginInstance]}  // Add toolbar with zooming support
                 />
             </Worker>
         </div>
